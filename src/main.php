a5b3acc3513c1ef9edbbc7f3131cb7d9019a22da
Razvan Tanase <tangent.jotey@gmail.com>
2023-01-27 13:53:21 +0100

    Version 0.4.136

--- src/main.php
+++ src/main.php
@@ -1,7 +1,13 @@
 <?php
 
+use function Amp\File\isDirectory;
+use function CatPaw\deleteDirectoryRecursively;
+
 use CatPaw\Web\Attributes\StartWebServer;
 
 #[StartWebServer]
 function main() {
+    if (yield isDirectory('.cache')) {
+        yield deleteDirectoryRecursively('.cache');
+    }
 }
